728e4c34eb549924abb26c0754427368d191cac1
Alex Lee <cine0831@gmail.com>
2022-12-12 02:18:07 +0900

    update GKE cluster standard mode

--- gke-cluster-standard.tf
+++ gke-cluster-standard.tf
@@ -1,19 +1,51 @@
 data "google_container_engine_versions" "this" {
   project        = var.project_id
   location       = var.cluster_location_type
-  version_prefix = "1.24."
+  version_prefix = var.kubernetes_version
 }
 
 resource "google_container_cluster" "gke_cluster_standard" {
-  name                     = var.cluster_name
-  location                 = var.cluster_location_type
-  node_locations           = var.node_locations
-  network                  = "projects/${var.project_id}/global/networks/${var.vpc_network}"
-  subnetwork               = var.subnetwork
+####################
+# Cluster basics
+####################
+  name                 = var.cluster_name
+  description          = var.cluster_description
+  location             = var.cluster_location_type
+  node_locations       = var.node_locations
   
+  release_channel {
+    channel = var.release_channel
+  }
+
+  #min_master_version = data.google_container_engine_versions.this.default_cluster_version
+  min_master_version = "1.24.5-gke.600"
+
+
+####################
+# Networking
+####################
+  network          = "projects/${var.project_id}/global/networks/${var.vpc_network}"
+  subnetwork       = var.subnetwork
+
+  private_cluster_config {
+    enable_private_endpoint = var.enable_private_endpoint
+    enable_private_nodes    = var.enable_private_nodes  
+    master_ipv4_cidr_block  = var.master_ipv4_cidr_block
+
+    master_global_access_config {
+      enabled = var.master_global_access_config
+    }
+  }
+
+  #default_snat_status {
+  #  disabled = 
+  #} 
+
+  networking_mode  = var.networking_mode   # VPC_NATIVE or ROUTES
+
   ip_allocation_policy {
-    cluster_ipv4_cidr_block  = var.cluster_ipv4_cidr_block
-    services_ipv4_cidr_block = var.services_ipv4_cidr_block
+    cluster_ipv4_cidr_block       = var.cluster_ipv4_cidr_block
+    services_ipv4_cidr_block      = var.services_ipv4_cidr_block
   }
 
   network_policy {
@@ -21,19 +53,17 @@ resource "google_container_cluster" "gke_cluster_standard" {
     provider = "PROVIDER_UNSPECIFIED"
   }
 
-  networking_mode = "VPC_NATIVE"
   datapath_provider = var.datapath_provider ? "ADVANCED_DATAPATH" : "DATAPATH_PROVIDER_UNSPECIFIED"
 
-  enable_tpu       = false
+  master_authorized_networks_config {
+    cidr_blocks {
+      #cidr_block   = lookup(cidr_blocks.value, "cidr_block", "")
+      #display_name = lookup(cidr_blocks.value, "display_name", "")
+      cidr_block   = var.master_authorized_networks_cidr_block
+      display_name = var.master_authorized_networks_display_name
 
-  private_cluster_config {
-    enable_private_endpoint = false
-    enable_private_nodes    = false
-
-    master_global_access_config {
-        enabled = false
     }
-}
+  }
 
 
   binary_authorization {
@@ -45,11 +75,4 @@ resource "google_container_cluster" "gke_cluster_standard" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
-
-  release_channel {
-    channel = "UNSPECIFIED"
-  }
-
-  #min_master_version = data.google_container_engine_versions.this.default_cluster_version
-  min_master_version = "1.24.5-gke.600"
 }
--- variables.tf
+++ variables.tf
@@ -8,6 +8,10 @@ variable "region" {
     type        = string
 }
 
+
+####################
+# Cluster basics
+####################
 variable "cluster_name" {
     description = "The name of the cluster, unique within the project and location."
     type        = string
@@ -28,6 +32,20 @@ variable "node_locations" {
     type        = list
 }
 
+variable "release_channel" {
+  description = "The release channel of this cluster. Accepted values are `UNSPECIFIED`, `RAPID`, `REGULAR` and `STABLE`. Defaults to `UNSPECIFIED`."
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "The Kubernetes version of the masters."
+  type        = string
+}
+
+
+####################
+# Networking
+####################
 variable "vpc_network" {
     description = "The name or self_link of the Google Compute Engine network to which the cluster is connected. For Shared VPC, set this to the self link of the shared network."
     type        = string
@@ -38,27 +56,58 @@ variable "subnetwork" {
     type        = string
 }
 
+variable "enable_private_endpoint" {
+  description = "When true, the cluster's private endpoint is used as the cluster endpoint and access through the public endpoint is disabled. When false, either endpoint can be used."
+  type        = bool
+}
+
+variable "enable_private_nodes" {
+  description = "Enables the private cluster feature, creating a private endpoint on the cluster. In a private cluster, nodes only have RFC 1918 private addresses and communicate with the master's private endpoint via private networking."
+  type        = bool
+}
+
+variable "master_ipv4_cidr_block" {
+  description = "The IP range in CIDR notation to use for the hosted master network. This range will be used for assigning private IP addresses to the cluster master(s) and the ILB VIP. This range must not overlap with any other ranges in use within the cluster's network, and it must be a /28 subnet."
+  type        = string
+}
+
+variable "master_global_access_config" {
+  description = "Whether the cluster master is accessible globally or not."
+  type        = bool
+}
+
+variable "networking_mode" {
+  description = "Determines whether alias IPs or routes will be used for pod IPs in the cluster. Options are VPC_NATIVE or ROUTES."
+  type        = string
+}
+
 variable "cluster_ipv4_cidr_block" {
     description = "The name of the existing secondary range in the cluster's subnetwork to use for pod IP addresses."
     type        = string
 }
-
-# variable "cluster_secondary_range_name" {
-#     description = "The IP address range of the Kubernetes pods in this cluster in CIDR notation (e.g. 10.96.0.0/14). Leave blank to have one automatically chosen or specify a /14 block in 10.0.0.0/8."
-#     type        = string
-# }
 
 variable "services_ipv4_cidr_block" {
     description = "The IP address range for the cluster pod IPs. Set to blank to have a range chosen with the default size. Set to /netmask (e.g. /14) to have a range chosen with a specific netmask. Set to a CIDR notation (e.g. 10.96.0.0/14) from the RFC-1918 private networks (e.g. 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16) to pick a specific range to use."
     type        = string
 }
 
-# variable "services_secondary_range_name" {
-#     description = " The name of the existing secondary range in the cluster's subnetwork to use for service ClusterIPs. Alternatively, services_ipv4_cidr_block can be used to automatically create a GKE-managed one."
-#     type        = string
-# }
-
 variable "datapath_provider" {
   description = "The desired datapath provider for this cluster. By default, `DATAPATH_PROVIDER_UNSPECIFIED` enables the IPTables-based kube-proxy implementation. `ADVANCED_DATAPATH` enables Dataplane-V2 feature."
   type        = bool
+}
+
+# variable "master_authorized_networks" {
+#   description = "List of master authorized networks. If none are provided, disallow external access (except the cluster node IPs, which GKE automatically whitelists)."
+#   type        = list(object({ cidr_block = string, display_name = string }))
+# }
+
+variable "master_authorized_networks_cidr_block" {
+  description = "test"
+  type        = list
+}
+
+variable "master_authorized_networks_display_name" {
+  description = "test"
+  type        = list
+
 }
