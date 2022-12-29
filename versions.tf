e38278757aaa05f85804016d85b5345f00a4498e
이장재 <ljj@wemakeprice.com>
2022-12-29 11:42:24 +0900

    update v0.0.1

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "1.3.6"
+  required_version = ">= 1.3.6, < 1.4.0"
 
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "4.46.0"
+      source  = "hashicorp/google"
+      version = ">= 4.46.0, < 5.0"
     }
 
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.2.1"
     }
   }
